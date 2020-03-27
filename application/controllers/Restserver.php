ed24a6ddbc1e5c11f98f4ff42fb769581c864fab
JuanDelgadillo <jsebastian.delgadillo@gmail.com>
2020-03-27 16:50:09 -0300

    add functions of get courses width pagination and get courses all

--- application/controllers/Restserver.php
+++ application/controllers/Restserver.php
@@ -7,12 +7,61 @@ class Restserver extends REST_Controller {
 
 	 public function __construct() {
        parent::__construct();
-       $this->load->database();
+       // $this->load->database();
+        // load the library api
+       $this->load->library('pagination');
+       // load the model page
+       $this->load->model('Restapi_model');
     }
 
-	public function courses_get()
+    /**
+	*	Get All data from courses
+    **/
+
+	public function courses_get( $perPage = 3  )
 	{
-		$array = array('Hola', 'Mundo', 'Codeginiter');
+		$dataPerPage = array();
+		$data = array();
+
+		if ( $perPage === 'all') {
+
+			$data['courses'] = $this->Restapi_model->get_courses();
+			
+			
+		}else{
+		
+			$cantReg = $this->Restapi_model->get_courses_count();
+			$cantPag = ceil($cantReg / $perPage);
+			$dats 	= array();
+			$page 	= 1;
+			$start 	= 0;
+
+			for ($a=0; $a < $cantPag; $a++) {
+
+				$dataPerPage['page_'.$page] = $this->Restapi_model->get_courses_limit( $start, $perPage );
+				$start 	= $start+$perPage;
+				$page++;
+
+			}
+
+			$data['courses'] = $dataPerPage;
+
+		}
+
+		$this->response($data);
+		
+		
+	}
+
+	/**
+	*	Get All data from course specific width id
+    **/
+		public function course_get( $id = 0 )
+	{
+		
+		$array = array('no hay', 'datos', 'encontrados', $id);
 		$this->response($array);
 	}
+
+
 }
---
+++ application/models/Restapi_model.php
@@ -0,0 +1,35 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Restapi_model extends CI_Model {
+
+	 function __construct() { 
+        // Set table name 
+        $this->table = 'course'; 
+    } 
+
+	public function get_courses(){
+		$this->db->select('*');
+		$this->db->from('course');
+		$query = $this->db->get();
+		return $query->result();
+	}
+
+	public function get_courses_limit( $start, $limit ){
+		$this->db->select('*');
+		$this->db->from('course');
+		$this->db->limit($limit, $start);
+		$query = $this->db->get();
+		return $query->result();
+	}
+
+	public function get_courses_count(){
+		$this->db->select('*');
+		$this->db->from('course');
+		$query = $this->db->get();
+		return $query->num_rows();
+	}
+	  
+}
+
+
